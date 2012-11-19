24f80e76ab34414f278202ea207e2f96c891617a
Alejandro Isaza <alejandro.isaza@gmail.com>
2012-11-18 23:34:31 -0800

    Add test for constant-speed line as bezier

--- svg/tests/bezier_test.cpp
+++ svg/tests/bezier_test.cpp
@@ -6,15 +6,27 @@
 
 #include <boost/test/unit_test.hpp>
 #include <boost/test/floating_point_comparison.hpp>
-
+#include <iostream>
+#include <iomanip>
 
 namespace mb {
 	namespace test {
 		BOOST_AUTO_TEST_CASE(eval) {
+			// a curve having controls points at the corners of a unit square
 			bezier b(point(0, 0), point(0, 1), point(1, 1), point(1, 0));
 			BOOST_CHECK_SMALL(distance(b.eval(0.f), point(0.f, 0.f)), 1e-15f);
 			BOOST_CHECK_SMALL(distance(b.eval(1.f), point(1.f, 0.f)), 1e-15f);
 			BOOST_CHECK_SMALL(distance(b.eval(.5f), point(.5f, .75f)), 1e-15f);
+			
+			// a constant-speed line segment
+			b.p1 = point(0, 0);
+			b.p2 = point(1, 0);
+			b.p3 = point(2, 0);
+			b.p4 = point(3, 0);
+			
+			// x coordinates must match the parameter times the length at every point
+			for (bezier::real_t t = 0; t <= 1; t += 0.1f)
+				BOOST_CHECK_SMALL(distance(b.eval(t), point(3*t, 0)), 1e-6f);
 		}
 		
 		BOOST_AUTO_TEST_CASE(split) {
