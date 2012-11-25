e009a40ba9c1bc57e2bc3e46c4c81387d5b41d39
Alejandro Isaza <alejandro.isaza@gmail.com>
2012-11-25 14:29:19 -0800

    Add signup validation and tests

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121123033854) do
+ActiveRecord::Schema.define(:version => 20121125221355) do
 
   create_table "malls", :force => true do |t|
     t.string   "name"
@@ -33,5 +34,7 @@ ActiveRecord::Schema.define(:version => 20121123033854) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
