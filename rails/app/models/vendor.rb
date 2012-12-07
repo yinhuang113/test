f3ac9da47cae0220bd7993938a679cc158e61b34
Alejandro Isaza <alejandro.isaza@gmail.com>
2012-12-06 21:45:38 -0800

    Add simple_form gem, edit vendor categories

--- rails/app/models/vendor.rb
+++ rails/app/models/vendor.rb
@@ -1,5 +1,5 @@
 class Vendor < ActiveRecord::Base
-  attr_accessible :contact_email, :description, :logo, :name, :website
+  attr_accessible :contact_email, :description, :logo, :name, :website, :category_ids
   has_and_belongs_to_many :categories, class_name: 'VendorCategory', association_foreign_key: 'category_id'
   validates :name, presence: true
 end
