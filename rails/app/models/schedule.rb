b17a1975e398b083118c700460084593bd9caeb0
Alejandro Isaza <alejandro.isaza@gmail.com>
2012-12-17 15:05:25 -0800

    Add schedule scopes

--- rails/app/models/schedule.rb
+++ rails/app/models/schedule.rb
@@ -5,4 +5,11 @@ class Schedule < ActiveRecord::Base
   validates :priority, presence: true, numericality: { only_integer: true }
   validates :name, presence: true
   validates :start_date, presence: true
+  
+  scope :by_date, proc { |date| where('(start_date IS NULL OR start_date <= ?) AND (end_date IS NULL OR end_date > ?)', date, date) }
+  
+  def self.current(mall)
+    date = Time.zone.now
+    where('mall_id = ? AND (start_date IS NULL OR start_date <= ?) AND (end_date IS NULL OR end_date > ?)', mall.id, date, date).order('priority DESC').first
+  end
 end
