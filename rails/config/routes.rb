98935098f387a33caee2bd06294ffafd27a16898
Peng <miconj96@gmail.com>
2013-01-11 11:35:37 +0800

    Append venues page and mapbuilder page.   - changed these pages     routes.rb     malls_controller.rb     index.hmtl.erb   - created javascript files and css files   - created new pages     views/malls/venu.html.erb, views/malls/mapbuilder.html.erb, views/layouts/mapbuilder.html.erb, views/layouts/_header_template1.html.erb

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -4,6 +4,8 @@ Mapbuilder::Application.routes.draw do
   match 'help', :to => 'static_pages#help'
   match '/about', :to => 'static_pages#about'
   match '/contact', :to => 'static_pages#contact'
+  match '/malls/venu', :to => 'malls#venu', :as => 'venu_mall'
+  match '/malls/mapbuilder', :to => 'malls#mapbuilder', :as => 'mapbuilder_mall'
   
   devise_for :users
   
