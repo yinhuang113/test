e5b9a7f67fd57cb067df79dc0efcf848c40681a9
Alejandro Isaza <alejandro.isaza@gmail.com>
2013-01-21 14:35:17 -0800

    Show error reports on staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -7,12 +7,9 @@ Mapbuilder::Application.configure do
   config.action_mailer.delivery_method = :sendmail
 
 
-  # Code is not reloaded between requests
   config.cache_classes = true
-
-  # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
