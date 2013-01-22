4be9d202b281d80fc86a707162446b859e68617c
Anton Petrunich <anton.petrunich@gmail.com>
2013-01-22 18:23:52 +0200

    Fix mapbuilder layout Start CSS reorganization

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def current_account
     current_user.try(:account)
   end
+
+  def body_class
+    %|l-#{current_layout} c-#{controller.controller_name} a-#{controller.action_name}|
+  end
+
+  def current_layout
+    controller.send :_layout
+  end
 end
