d6c976a9edf3803856189d8ad11fc97b39317154
Alejandro Isaza <alejandro.isaza@gmail.com>
2013-01-26 11:45:37 -0800

    Fix production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Mapbuilder::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( jquery.checkbox.css )
+  config.assets.precompile += %w( jquery.checkbox.css mapbuilder.css )
   
   # Enable threaded mode
   # config.threadsafe!
