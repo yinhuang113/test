209e967107890d1d79f2fe7578990bb4e6252a92
Andrew Zinenko <zinenkoan@gmail.com>
2013-02-06 01:35:20 +0400

    move scripts to vendor and fix profile page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,19 +1,21 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery
 //= require jquery_ujs
-//= require_tree ../../../vendor/assets/javascripts
-//= require_tree ./js
-//= require profile/jquery.checkbox.min
+
+//= require twitter/bootstrap
+
+//= require bootstrap-limit
+//= require bootstrap-datepicker
+//= require bootstrap-select
+//= require bootstrap-timepicker
+
+//= require jquery-ui-1.9.2.custom.min
+//= require jquery.checkbox.min
+//= require jquery.idTabs.min
+//= require jquery.jscrollpane
+//= require jquery.mousewheel
+//= require santiscroll
+
 //= require connectors
 //= require floors
+
+//= require d3.v2
--- app/assets/javascripts/profile.js
+++
@@ -1,5 +0,0 @@
-//= require jquery
-//= require jquery_ujs
-//= require_tree ../../../vendor/assets/javascripts
-//= require profile/jquery.checkbox.min
-//= require_tree ./js
